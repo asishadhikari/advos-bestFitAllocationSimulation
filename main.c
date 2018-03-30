41366d29364d580a5c3f88dafb4eeb6909254fa4
sikari <ashish.adhikari@bison.howard.edu>
2018-03-29 22:41:55 -0400

    best fit remaining;

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 /* Best fit algorithm simulation
 	Ashish Adhikari
 	run as : ./sim 9   //for 9 processes
+
 */
 
 #include <stdlib.h>
@@ -8,34 +9,46 @@
 #include <math.h>
 #include <unistd.h>
 #include <sys/time.h>
-//mem size in bytes
-#define MEMORY_SIZE (int)pow(2,22)
-#define BLOCK_SIZE (int)pow(2,10)	//1 kb
+#define MEMORY_SIZE (int)pow(2,22)	//4MB
+#define BLOCK_SIZE (int)pow(2,10)	//1 KB
 //stop simulation these many allocations ??
 #define REQUIRED_ALLOCATIONS (int)pow(2,5)
-
+//for easy use of max index of memory
 #define MAX_INDEX MEMORY_SIZE/BLOCK_SIZE
-
+//generate N processes of rand()%91+10 KB 
 void generate_procs(int N);
+//allocate memory to each process free_memory() 
 void allocate_proc(int batch, int size);
 //start index of suitable hole
 int find_best_index(int size);
 //deallocate N oldest processes
 void free_memory(void);
-int available_hole(void);
+//return size of available hole
+int total_available_hole(void);
 
-//memory bitmap
-int *MEMORY;
-//num of processes allocated memory
+
+/*memory arranged as [1,-1,4,2,-1]
+	edge cases:
+		- when process batch is 4 and memory size is 2,
+			we cannot allocate the last index as -1
+		- when batch is 0, we must check that value
+			on next index is greater than 9 to make sure 
+			we don't skip it
+		- when batch size is 0, similar problem arises 
+			so check for next index here as well.
+*/
+int *MEMORY;	
 int num_allocated=0;
 int batch = 0;  //process batch number
 int min_batch = 0; //oldest process batch number 
 
 int main(int argc, char *argv[]){
+	//invalid input handling
 	if (argc==1 || argc > 2){
 		printf("Invalid number of parameters!!\n");
 		exit(-1);
 	}
+	//not handled when string is provided ??
 	int N = atoi(argv[1]);
 	
 	//initialise memory 
@@ -47,60 +60,72 @@ int main(int argc, char *argv[]){
 	gettimeofday(&seed,NULL);
 	srand(seed.tv_usec);
 	//simulate until crtiteria met 
-	while(num_allocated<REQUIRED_ALLOCATIONS){
+	while(num_allocated<REQUIRED_ALLOCATIONS)
 		generate_procs(N);
-	}
 	return 0;
 } 
-
+/*generates N processes and and free_memory() 
+	until enough memory is available*/ 
 void generate_procs(int N){
 	int total_size = 0;
+	//batch is global batch
 	int batch_num = batch;
+	//[pid][size]
 	int process[N][1];
 	for (int i = 0; i < N; i++){
 		int size = rand()%91+10;
 		total_size+=size;
 		process[i][0] = size;
 	}
-	while (available_hole()<total_size)
+	//self explanatory :) 
+	while (total_available_hole()<total_size)
 		free_memory();
-	
+	//allocate memory for each process 
 	for (int i = 0; i < N; i++)
 		allocate_proc(batch_num,process[i][0]);
-
+	//make sure batch is consistent with code
 	batch++;
 
 }
 
 void allocate_proc(int batch_num, int size){
 	int start = find_best_index(size);
+	//refer to comment at top to understand memory layout
 	MEMORY[start] = batch_num;
 	MEMORY[start+1]= size;
 	MEMORY[start+size-1] = -1;
 	for (int i = start+2; i <start+size-2; i++)
-		MEMORY[i] = 1;
-
+		MEMORY[i] = 1; //fill used up memory blocks with 1
+	num_allocated++; 
 }
 //free the oldest batch processes
 void free_memory(){
 	for (int i = 0; i <MAX_INDEX; i++){
+		/*
+			Memory looks something like:
+		[...,1,-1,4,12,1,1,1,1,1,1,1,1,1,-1,0,0,0,3,61,1,...]
+		
+		*/
 		if(MEMORY[i]==min_batch && MEMORY[i+1]>9){
-			for (int j = i; j < MEMORY[i+1]; j++)
+			for (int j = i; j < i+MEMORY[i+1]-1; j++)
 				MEMORY[j] = 0;
 		}
 	}
 	min_batch++;
 }
 
-//return start index of 
+//return start index acc to best fit algorithm 
 int find_best_index(int size){
-
+	
 }
 
-int available_hole(){
-	int hole = 0;
+
+int total_available_hole(){
+	int hole_size = 0; //1 block = 1 hole
 	for (int i = 0; i < MAX_INDEX; i++)
 		if(MEMORY[i]==0 && MEMORY[i+1]==0)
-			hole+=1;
-	return hole;
+			hole_size+=1;
+	return hole_size;
 }
+
+
